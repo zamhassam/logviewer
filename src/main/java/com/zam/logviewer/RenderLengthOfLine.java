68196136cce056d3365fdcd1812d6c1ada866a37
zamhassam <zam.hassam@googlemail.com>
2018-08-23 21:41:32 +0100

    Enum conversion

--- src/main/java/com/zam/logviewer/RenderLengthOfLine.java
+++ src/main/java/com/zam/logviewer/RenderLengthOfLine.java
@@ -1,7 +1,5 @@
 package com.zam.logviewer;
 
-import com.sun.org.apache.xpath.internal.jaxp.XPathExpressionImpl;
-import com.sun.org.apache.xpath.internal.jaxp.XPathImpl;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -11,30 +9,47 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.*;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 
 public class RenderLengthOfLine implements BottomPaneRenderer
 {
     private final Map<Integer, String> fields = new HashMap<>();
+    private final Map<Integer, Map<String, String>> enums = new HashMap<>();
 
     public RenderLengthOfLine(String fixFileLocation)
     {
         try
         {
-            XPathExpression xPathExpression = XPathFactory.newInstance().newXPath().compile("/fix/fields/field");
-            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            Document document = documentBuilder.parse(new File(fixFileLocation));
-            NodeList nodeList = (NodeList) xPathExpression.evaluate(document, XPathConstants.NODESET);
+            final XPathExpression xPathExpression = XPathFactory.newInstance().newXPath().compile("/fix/fields/field");
+            final DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            final DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            final Document document = documentBuilder.parse(new File(fixFileLocation));
+            final NodeList nodeList = (NodeList) xPathExpression.evaluate(document, XPathConstants.NODESET);
             forEach(nodeList, node ->
             {
-                String name = node.getAttributes().getNamedItem("name").getNodeValue();
-                int number = Integer.parseInt(node.getAttributes().getNamedItem("number").getNodeValue());
+                final String name = node.getAttributes().getNamedItem("name").getNodeValue();
+                final int number = Integer.parseInt(node.getAttributes().getNamedItem("number").getNodeValue());
                 fields.put(number, name);
+                if (node.hasChildNodes())
+                {
+                    forEach(node.getChildNodes(), childNode ->
+                    {
+                            final String enumName = childNode.getAttributes().getNamedItem("description").getNodeValue();
+                            final String enumValue = childNode.getAttributes().getNamedItem("enum").getNodeValue();
+                            Map<String, String> enumMap = enums.computeIfAbsent(number, HashMap::new);
+                            enumMap.put(enumValue, enumName);
+                    });
+                }
             });
         }
         catch (ParserConfigurationException | SAXException | IOException | XPathExpressionException e)
@@ -57,17 +72,17 @@ public class RenderLengthOfLine implements BottomPaneRenderer
 
     List<String> renderBottomPaneContents(String currentLine)
     {
-        List<String> rows = new ArrayList<>();
+        final List<String> rows = new ArrayList<>();
         for (String keyValue : currentLine.split("\\\\001"))
         {
-            String[] keyValSplit = keyValue.split("=");
+            final String[] keyValSplit = keyValue.split("=");
             if (keyValSplit.length != 2)
             {
                 rows.add(keyValue);
                 continue;
             }
 
-            int key;
+            final int key;
             try
             {
                 key = Integer.parseInt(keyValSplit[0]);
@@ -78,9 +93,25 @@ public class RenderLengthOfLine implements BottomPaneRenderer
                 continue;
             }
 
-            String keyRepr = fields.getOrDefault(key, keyValSplit[0]);
-            rows.add(keyRepr + "[" + keyValSplit[0] + "] = " + keyValSplit[1]);
+            final String val;
+            if (enums.containsKey(key) && enums.get(key).containsKey(keyValSplit[1]))
+            {
+                val = enums.get(key).get(keyValSplit[1])  +"[" + keyValSplit[1] + "]";
+            }
+            else
+            {
+                val = keyValSplit[1];
+            }
+
+            final String keyRepr = fields.getOrDefault(key, keyValSplit[0]);
+            rows.add(keyRepr + "[" + keyValSplit[0] + "] = " + val);
         }
         return rows;
+    }
+
+    public static void main(String[] args)
+    {
+        RenderLengthOfLine renderer = new RenderLengthOfLine("src/main/resources/FIX42.xml");
+        System.out.println(renderer.enums);
     }
 }
