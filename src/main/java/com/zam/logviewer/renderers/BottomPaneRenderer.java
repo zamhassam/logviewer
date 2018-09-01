1dbb372c0f574006d3803f732de2a877c0ff91d1
zamhassam <zam.hassam@googlemail.com>
2018-09-01 16:25:45 +0100

    Added comments

--- src/main/java/com/zam/logviewer/renderers/BottomPaneRenderer.java
+++ src/main/java/com/zam/logviewer/renderers/BottomPaneRenderer.java
@@ -4,5 +4,8 @@ import java.util.List;
 
 public interface BottomPaneRenderer
 {
+    /**
+     * Given the current input line from the top pane, return an ordered list of lines to render in the bottom  pane
+     */
     List<String> renderBottomPaneContents(String currentLine);
 }
--- src/main/java/com/zam/logviewer/terminallines/TerminalLines.java
+++ src/main/java/com/zam/logviewer/terminallines/TerminalLines.java
@@ -4,19 +4,43 @@ import java.util.Optional;
 
 public interface TerminalLines
 {
+    /**
+     * What is the top line for the terminal screen
+     */
     Node getTopLineNode();
 
+    /**
+     * Set the new top line for this terminal screen
+     */
     void setTopLineNode(Node topLineNode);
 
+    /**
+     * What is the bottom line for the terminal screen
+     */
     Node getBottomLineNode();
 
+    /**
+     * Set the new bottom line for this terminal screen
+     */
     void setBottomLineNode(Node bottomLineNode);
 
+    /**
+     * The next node to the provided
+     */
     Optional<Node> nextNode(Node node);
 
+    /**
+     * The previous node to the provided
+     */
     Optional<Node> prevNode(Node node);
 
+    /**
+     * What is the current line for the terminal screen
+     */
     Node getCurrentLineNode();
 
+    /**
+     * Set the new current line for this terminal screen
+     */
     void setCurrentLineNode(Node currentLineNode);
 }
